2f3ce22fa14a6a02a29e693d8f4771de2e3197e6
Robin <robin888b@outlook.com>
2022-03-04 18:18:09 +0100

    _

--- src/app.js
+++ src/app.js
@@ -1,9 +1,50 @@
+
 function menuToggle(){
     var nav = document.getElementById("nav")
     nav.classList.toggle("active")
     var toggle = document.getElementById("toggle")
     toggle.classList.toggle("active")
 }
+
+fetch("./modalContent.json").then(res => 
+    console.log(res.title[1])
+)
+function toggleModal(type){
+    var mondalContainer = document.querySelector(".modal-container")
+    var modalContent = document.getElementById("modal-content")
+    var modalTitle = document.getElementById("modal-title")
+    
+    modalTitle.textContent = modaltextjson.title[type]
+
+    modalContent.innerHTML = modaltextjson.content[type]
+    
+    mondalContainer.classList.add("active")
+    console.log(`Show modal ${type}`)
+}
+
+
+/*/ modal type :
+
+ 0 : Discord -> anglais
+ 1 : Gay Def -> anglais
+ 2 : Fur Def -> anglais
+ - : -------------------
+ 3 : Discord -> français
+ 4 : Gay Def -> français
+ 5 : Fur Def -> français
+
+*/
+
+
+
+
+function closeModal(){
+    var mondalContainer = document.querySelector(".modal-container")
+    mondalContainer.classList.remove("active")
+    console.log(`hide modal`)
+}
+
+
 
 function toggleSpoiler(id) {
     let spoiler = document.getElementById(id)
@@ -32,7 +73,7 @@ const getCountDown = () => {
     if (textYo){
         textYo.textContent = `I'm actualy 17Yo but i will be 18 in ${nbDay} day and ${nbHoursWithoutDay} hours.`
     } else {
-        textage.textContent = `j'ais actuellement 17 ans mais j'en aurais 18 dans ${nbDay} jours et ${nbHoursWithoutDay} heures.`
+        textage.textContent = `j'ai actuellement 17 ans mais j'en aurai 18 dans ${nbDay} jours et ${nbHoursWithoutDay} heures.`
     }
     if (remainingTime > 0) { clearInterval(countDownInterval) }
 }
