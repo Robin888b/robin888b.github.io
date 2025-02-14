2f7250082f9f7cdf772b14eb63ad198c257928f5
Raven <robin888b@outlook.com>
2025-02-14 17:39:23 +0100

    starting the computer version

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,19 @@
 // Spoiler
 document.querySelectorAll(".spoiler").forEach(function(e){e.addEventListener("click", ()=> {e.classList.add("clicked")})})
 
+// Mobile menu
+let toggle = false
+let menu = document.getElementById("headLinkList")
+document.getElementById("menuToggle").addEventListener("click", e=>{
+
+    if (toggle) {
+        menu.classList.remove("active")
+        toggle = false
+    } else {
+        menu.classList.add("active")
+        toggle = true
+    }
+})
 
 
 
