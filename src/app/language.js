8e9f114688e6b7baa81b09634e5f4074d6ae13ae
Raven Spart • Robin888b <robin888b@outlook.com>
2025-02-18 23:53:30 +0100

    add multiple language

---
+++ src/app/language.js
@@ -0,0 +1,56 @@
+const lang = {
+    "en": {
+        "header": ["Home","Links","Gallery"],
+        "presentation": `<h1>Raven Spart</h1>
+            <p>Hello 👋<br>
+                My name is Raven; I'm a <span id="age">20</span>yo french student in informatic license; <br><br>
+                for me, programing is often like a game, <span class="spoiler" id="sp1">when you don't ask me to use Ocaml or assembly 👀</span> <br><br>
+                I enjoy photography in my spare time, and sometimes I draw furries stuff <br><br>
+                I'm also a gay furry who likes to play justDance and spends all his time listening to music
+            </p>`,
+        "links": [
+            "Where i post my photos",
+            "To send me privates messages",
+            "Everything and anything, all of the time",
+            "Like twitter, but doesn't belong to a nazi",
+            "The code of my open-sources projects",
+            "Only for people I know <br><code>@Robin888b</code>"
+        ],
+    },
+    "fr": {
+        "header": ["Présentation","Liens","Gallerie"],
+        "presentation": `<h1>Raven Spart</h1>
+            <p>Salut 👋<br>
+                Je m'appelle Raven; je suis un étudian français en licence d'informatique de <span id="age">20</span> ans; <br><br>
+                pour moi, la programmation est comme un jeu, <span class="spoiler" id="sp1">quand tu ne me demande pas d'utiliser l'Ocaml ou l'assembleur 👀</span><br><br>
+                J'aimeprendre des photos sur mon temps libre et parfois je dessinne des trucs furries<br><br>
+                je suis aussi un furry gay qui aime participer au JustDance en conv et passe tout son temps à écouter de la musique
+            </p>`,
+        "links": [
+            "Où je poste photos",
+            "Pour m'envoyer des messages",
+            "Tout et n'importe quoi, à tout moment",
+            "Comme twitter mais n'appartient pas à un nazi",
+            "Le code de mes projets open-sources",
+            "Seulement pour les personnes que je connais<br><code>@Robin888b</code>"
+        ],
+    },
+    "no": {
+        "header": ["Presentasjon","Nettverks","galleri"],
+        "presentation": `<h1>Raven Spart</h1>
+            <p>Hei 👋<br>
+                Jeg heter Raven, jeg er en <span id="age">20</span> år gammel fransk student i informatisk lisens; <br><br>
+                For meg er programmering ofte som et spill, <span class="spoiler" id="sp1">når du ikke ber meg om å bruke Ocaml eller assembly 👀</span> <br><br>
+                Jeg liker å fotografere på fritiden, og noen ganger tegner jeg furries-ting <br><br>
+                Jeg er også en homofil furry som liker å spille justDance og bruker all sin tid på å høre på musikk
+            </p>`,
+        "links": [
+            "Hvor jeg legger ut bildene mine",
+            "For å sende meg private meldinger",
+            "Alt og hva som helst, hele tiden",
+            "Som twitter, men tilhører ikke en nazist",
+            "Koden til open source-prosjektene mine",
+            "Bare for folk jeg kjenner <br><code>@Robin888b</code>"
+        ],
+    },
+}
